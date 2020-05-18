894ed230ba4aa2a015fad551cb46f376784e99bb
felipefbs <felipeffbs3x@gmail.com>
2020-05-18 18:01:19 -0300

    refact: added width and height to piece models

--- 2020-QuarentenaTech/os-3-pontinhos/src/constants.js
+++ 2020-QuarentenaTech/os-3-pontinhos/src/constants.js
@@ -13,10 +13,10 @@ const SHAPES = [
     [1, 0, 0],
 =======
 const I_MODEL = {
-  shape: [
-    [1, 1, 1, 1],
-  ],
+  shape: [[1, 1, 1, 1]],
   color: "#FFFF",
+  pieceWidth: 4,
+  pieceHeight: 1,
 };
 
 const J_MODEL = {
@@ -24,21 +24,25 @@ const J_MODEL = {
     [0, 0, 1],
 >>>>>>> 98c5595... refact: changing how to storage each piece data
     [1, 1, 1],
-    [0, 0, 0],
+    //[0, 0, 0],
   ],
   color: "#FFFF",
+  pieceWidth: 3,
+  pieceHeight: 2,
 };
 
 const L_MODEL = {
   shape: [
     [1, 0, 0],
     [1, 1, 1],
-    [0, 0, 0],
+    //[0, 0, 0],
   ],
 <<<<<<< HEAD
   [
 =======
   color: "#FFFF",
+  pieceWidth: 3,
+  pieceHeight: 2,
 };
 
 const O_MODEL = {
@@ -48,33 +52,41 @@ const O_MODEL = {
     [1, 1],
   ],
   color: "#FFFF",
+  pieceWidth: 2,
+  pieceHeight: 2,
 };
 
 const S_MODEL = {
   shape: [
     [0, 1, 1],
     [1, 1, 0],
-    [0, 0, 0],
+    //[0, 0, 0],
   ],
   color: "#FFFF",
+  pieceWidth: 3,
+  pieceHeight: 2,
 };
 
 const Z_MODEL = {
   shape: [
     [1, 1, 0],
     [0, 1, 1],
-    [0, 0, 0],
+    //[0, 0, 0],
   ],
   color: "#FFFF",
+  pieceWidth: 3,
+  pieceHeight: 2,
 };
 
 const T_MODEL = {
   shape: [
     [1, 1, 1],
     [0, 1, 0],
-    [0, 0, 0],
+    //[0, 0, 0],
   ],
   color: "#FFFF",
+  pieceWidth: 3,
+  pieceHeight: 2,
 };
 
 const MODELS = [I_MODEL, J_MODEL, L_MODEL, O_MODEL, S_MODEL, Z_MODEL, T_MODEL];
