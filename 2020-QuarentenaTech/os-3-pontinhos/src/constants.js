060a73c42dca1125a3c743cc930caa36e32b2691
Henrique Miranda <enrque.ns@gmail.com>
2020-05-19 18:45:29 -0300

    :bulb: chore: add colors

--- 2020-QuarentenaTech/os-3-pontinhos/src/constants.js
+++ 2020-QuarentenaTech/os-3-pontinhos/src/constants.js
@@ -14,33 +14,41 @@ const SHAPES = [
 =======
 const I_MODEL = {
   shape: [[1, 1, 1, 1]],
-  color: "#FFFF",
+  color: "#00ADEE",
   width: 4,
   height: 1,
 };
 
 const J_MODEL = {
   shape: [
+<<<<<<< HEAD
     [0, 0, 1],
 >>>>>>> 98c5595... refact: changing how to storage each piece data
+=======
+>>>>>>> ac264a2... :bulb: chore: add colors
     [1, 1, 1],
+    [1, 0, 0],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#F6921E",
   width: 3,
   height: 2,
 };
 
 const L_MODEL = {
   shape: [
-    [1, 0, 0],
     [1, 1, 1],
+    [0, 0, 1],
     //[0, 0, 0],
   ],
+<<<<<<< HEAD
 <<<<<<< HEAD
   [
 =======
   color: "#FFFF",
+=======
+  color: "#1B75BB",
+>>>>>>> ac264a2... :bulb: chore: add colors
   width: 3,
   height: 2,
 };
@@ -51,7 +59,7 @@ const O_MODEL = {
     [1, 1],
     [1, 1],
   ],
-  color: "#FFFF",
+  color: "#FFF100",
   width: 2,
   height: 2,
 };
@@ -62,7 +70,7 @@ const S_MODEL = {
     [1, 1, 0],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#8BC53F",
   width: 3,
   height: 2,
 };
@@ -73,7 +81,7 @@ const Z_MODEL = {
     [0, 1, 1],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#EC1C24",
   width: 3,
   height: 2,
 };
@@ -84,7 +92,7 @@ const T_MODEL = {
     [0, 1, 0],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#652D90",
   width: 3,
   height: 2,
 };
