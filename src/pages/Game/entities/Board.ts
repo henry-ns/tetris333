54a41fc636c4b0e089251f3c5e2a07cc6544fdbc
Henrique Miranda <enrque.ns@gmail.com>
2020-06-30 09:47:45 -0300

    :bulb: chore: remove useless varible on board

--- src/pages/Game/entities/Board.ts
+++ src/pages/Game/entities/Board.ts
@@ -37,8 +37,6 @@ class Board {
 
   points: number;
 
-  countPlays: number;
-
   constructor(
     private canvas: P5,
     private config: Omit<ConfigData, 'difficulty'>,
@@ -57,7 +55,6 @@ class Board {
 
     this.level = 1;
     this.points = 0;
-    this.countPlays = 0;
 
     this.displayPoints();
 
